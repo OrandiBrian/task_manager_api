824e71f53637b1d9150dff0209d7e4363da550db
Brian Orandi <brianorandi97@gmail.com>
2025-03-26 21:52:18 +0300

    Added CustomUserManager and CustomUser models

--- taskmanager/tasks/models.py
+++ taskmanager/tasks/models.py
@@ -1,5 +1,53 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.contrib.auth.models import UserManager, AbstractBaseUser, PermissionsMixin
+from django.conf import settings
+import uuid
+
+
+# creating custom user manager
+class CustomUserManager(UserManager):
+    def _create_user(self, name, email, password, **extra_fields):
+        if not email:
+            raise ValueError("The Email field must be set")
+        email = self.normalize_email(email)
+        user = self.model(name=name, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    # creating user
+    def create_user(self, name=None, email=None, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+
+        return self._create_user(name, email, password, **extra_fields)
+    
+    # create superuser
+    def create_superuser(self, name=None, email=None, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        return self._create_user(name, email, password, **extra_fields)
+    
+# creating custom user model
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(auto_now=True, blank=True, null=True)
+
+    objects = CustomUserManager()
+
+    # assigning email as username
+    USERNAME_FIELD = "email"
+    EMAIL_FIELD = "email"
+    REQUIRED_FIELDS = ["name"]
+
 
 # creating Task model
 class Task(models.Model):
@@ -21,7 +69,7 @@ class Task(models.Model):
     description = models.TextField()
     priority = models.CharField(max_length=6, choices=PRIORITY_CHOICES)
     status = models.CharField(max_length=9, choices=STATUS_CHOICES, default="Pending")
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="tasks")
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     completed_at = models.DateTimeField(null=True, blank=True)
 
     # string representation of the model
